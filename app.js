a41305b675d4ed8989eeaffb58ff429e1ac69397
Brian Chen <bchen.chen93@gmail.com>
2016-02-17 14:06:08 +1300

    feat: Youtube videos now show up and work as intended

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var myApp = angular.module('myApp',[]);
 myApp.controller('TimerController', ['$scope', function($scope) {
   $scope.startScreenBoolean = true;
   $scope.ytLink = "";
+  $scope.ytEmbedLink = "";
+  $scope.ytHeight = 390;
+  $scope.ytWidth = 640;
 
   $scope.startTimer = function() {
     $scope.startScreenBoolean = false;
@@ -13,6 +16,44 @@ myApp.controller('TimerController', ['$scope', function($scope) {
   };
 
   $scope.$watch('ytLink', function(newValue, oldValue) {
-    // Changes iframe on the dom to showcase the video linked
+    if (newValue == oldValue) {
+      return;
+    }
+
+    $scope.ytEmbedLink = $scope.ytLink.substr("https://www.youtube.com/watch?v=".length);
   });
 }]);
+
+myApp.directive('youtube', function($window) {
+  return {
+    restrict: "E",
+
+    scope: {
+      height:   "@",
+      width:    "@",
+      videoid:  "@"
+    },
+
+    template: '<div></div>',
+
+    link: function(scope, element) {
+      var tag = document.createElement('script');
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      var player;
+
+      $window.onYouTubeIframeAPIReady = function() {
+        player = new YT.Player(element.children()[0], {height: scope.height,
+          width: scope.width,
+          videoId: scope.videoid
+        });
+      };
+
+      scope.$watch('videoid', function(newValue, oldValue) {
+        player.cueVideoById(scope.videoid);
+      });
+    },
+  }
+});
