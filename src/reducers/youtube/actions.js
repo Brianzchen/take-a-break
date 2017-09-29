d269e47bc730078b1c84dfe69888404128d35415
Brian Chen <bzchen93@gmail.com>
2017-09-30 09:46:14 +1300

    moved links to reducer

--- src/reducers/youtube/actions.js
+++ src/reducers/youtube/actions.js
@@ -1,7 +1,4 @@
-import { clone } from 'lodash';
-
 import * as constants from './constants';
-import newLink from './objects/newLink';
 
 export const addOneToRepeat = () => ({
   type: constants.ADD_ONE_TO_REPEAT,
@@ -11,50 +8,22 @@ export const minusOneToRepeat = () => ({
   type: constants.MINUS_ONE_TO_REPEAT,
 });
 
-export const setLinks = links => ({
-  type: constants.SET_LINKS,
-  payload: links,
+export const addLink = index => ({
+  type: constants.ADD_LINK,
+  payload: index,
 });
 
-export const setLink = (link, index) => (
-  (dispatch, getState) => {
-    const links = clone(getState().youtube.links);
-    if (typeof links[index] !== 'undefined') {
-      links[index] = newLink(link);
-    } else {
-      links.push(newLink(link));
-    }
+export const removeLink = index => ({
+  type: constants.REMOVE_LINK,
+  payload: index,
+});
 
-    dispatch(setLinks(links));
-    localStorage.setItem('links', JSON.stringify(links));
-  }
-);
+export const setLink = (link, index) => ({
+  type: constants.SET_LINK,
+  payload: { link, index },
+});
 
-export const setLinkTitle = (title, index) => (
-  (dispatch, getState) => {
-    const links = clone(getState().youtube.links);
-    links[index].title = title;
-
-    dispatch(setLinks(links));
-  }
-);
-
-export const addLink = index => (
-  (dispatch, getState) => {
-    const links = clone(getState().youtube.links);
-    links.splice(index + 1, 0, newLink());
-
-    dispatch(setLinks(links));
-    localStorage.setItem('links', JSON.stringify(links));
-  }
-);
-
-export const removeLink = index => (
-  (dispatch, getState) => {
-    const links = clone(getState().youtube.links);
-    links.splice(index, 1);
-
-    dispatch(setLinks(links.length === 0 ? [newLink()] : links));
-    localStorage.setItem('links', JSON.stringify(links.length === 0 ? [''] : links));
-  }
-);
+export const setLinkTitle = (title, index) => ({
+  type: constants.SET_LINK_TITLE,
+  payload: { title, index },
+});
--- src/reducers/youtube/reducer.js
+++ src/reducers/youtube/reducer.js
@@ -1,27 +1,78 @@
+import clone from 'lodash/clone';
+
 import initialState from './initialState';
 import * as constants from './constants';
+import newLink from './objects/newLink';
 
 export default (state = initialState, action) => {
-  const canMinusRepeat = state.repeat > 0;
-
   switch (action.type) {
     case constants.ADD_ONE_TO_REPEAT:
       localStorage.setItem('repeat', state.repeat + 1);
+
       return {
         ...state,
         repeat: state.repeat + 1,
       };
-    case constants.MINUS_ONE_TO_REPEAT:
+    case constants.MINUS_ONE_TO_REPEAT: {
+      const canMinusRepeat = state.repeat > 0;
       canMinusRepeat && localStorage.setItem('repeat', state.repeat - 1);
+
       return {
         ...state,
         repeat: canMinusRepeat ? state.repeat - 1 : state.repeat,
       };
-    case constants.SET_LINKS:
+    }
+    case constants.ADD_LINK: {
+      const links = clone(state.links);
+      links.splice(action.payload + 1, 0, newLink());
+
+      localStorage.setItem('links', JSON.stringify(links));
+
       return {
         ...state,
-        links: action.payload,
+        links,
       };
+    }
+    case constants.REMOVE_LINK: {
+      const oldLinks = clone(state.links);
+      oldLinks.splice(action.payload, 1);
+
+      const links = oldLinks.length === 0 ? [newLink()] : oldLinks;
+      localStorage.setItem('links', JSON.stringify(links));
+
+      return {
+        ...state,
+        links,
+      };
+    }
+    case constants.SET_LINK: {
+      const { link, index } = action.payload;
+
+      const links = clone(state.links);
+      if (typeof links[link] !== 'undefined') {
+        links[index] = newLink(link);
+      } else {
+        links.push(newLink(link));
+      }
+
+      localStorage.setItem('links', JSON.stringify(links));
+
+      return {
+        ...state,
+        links,
+      };
+    }
+    case constants.SET_LINK_TITLE: {
+      const { title, index } = action.payload;
+
+      const links = clone(state.links);
+      links[index].title = title;
+
+      return {
+        ...state,
+        links,
+      };
+    }
     default:
       return state;
   }
